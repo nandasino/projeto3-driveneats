b4f6fdcc2eea1e353e1ac9f047afe685ad010de6
unknown <fernandamsino@gmail.com>
2022-06-12 17:29:52 -0300

    botão de fechar pedido finalizado

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 function selecionarPrato (elemento){
-    const itemClicado = document.querySelector(".selecionar");
+    let itemClicado = document.querySelector('.pratos .selecionar');
     if (itemClicado !==null){
-        itemClicado.classList.remove("selecionar");
+        itemClicado.classList.remove('selecionar');
     }
-    elemento.classList.add("selecionar");
+    elemento.classList.add('selecionar');
+    fecharPedido()
 }
 function selecionarBebida (elemento){
-    const itemClicado = document.querySelector(".bebidas .selecionar");
+    let itemClicado = document.querySelector('.bebidas .selecionar');
     if (itemClicado !==null){
-        itemClicado.classList.remove("selecionar");
+        itemClicado.classList.remove('selecionar');
     }
     elemento.classList.add("selecionar");
+    fecharPedido()
 }
 function selecionarSobremesa (elemento){
-    const itemClicado = document.querySelector(".sobremesas .selecionar");
+    let itemClicado = document.querySelector('.sobremesas .selecionar');
     if (itemClicado !==null){
-        itemClicado.classList.remove("selecionar");
+        itemClicado.classList.remove('selecionar');
     }
-    elemento.classList.add("selecionar");
+    elemento.classList.add('selecionar');
+    fecharPedido()
+}
+
+function fecharPedido(){
+    let pedidoFechado = document.querySelector('.botao');
+    let textoBotao = document.querySelector('.texto-botao');
+
+    let checarPratos = document.querySelector('.pratos .selecionar');
+    let checarBebidas = document.querySelector('.bebidas .selecionar');
+    let checarSobremesas = document.querySelector('.sobremesas .selecionar');
+
+    if(checarPratos && checarBebidas && checarSobremesas){
+    pedidoFechado.classList.add('fechar-pedido')
+    textoBotao.innerHTML = "Fechar pedido"
+    }
+}
+
+function abrirWhats(){
+    let checarPratos = document.querySelector('.pratos .selecionar');
+    let checarBebidas = document.querySelector('.bebidas .selecionar');
+    let checarSobremesas = document.querySelector('.sobremesas .selecionar');
+
+    if(checarPratos && checarBebidas && checarSobremesas){
+        const nomePrato = checarPratos.querySelector('.nome')
+        const nomeBebida = checarBebidas.querySelector('.nome')
+        const nomeSobremesa = checarSobremesas.querySelector('.nome')
+
+        const precoPrato = checarPratos.querySelector('.preco')
+        const precoBebida = checarBebidas.querySelector('.preco')
+        const precoSobremesa = checarSobremesas.querySelector('.preco')
+    }
 }
